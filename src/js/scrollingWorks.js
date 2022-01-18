3e208df733dd43dfafe9dd10ebc89716fa840301
Seregey_Maximov <dev9092336062@gmail.com>
2022-01-18 13:34:10 +0300

    added script for opacity works buttons

--- src/js/scrollingWorks.js
+++ src/js/scrollingWorks.js
@@ -1,23 +1,38 @@
-const scrollingWorks = () => {
-	const projects = document.querySelector('.section3Projects')
-	const leftBtn = document.querySelector('.section3BtnLeft')
-	const rightBtn = document.querySelector('.section3BtnRight')
-	const projectsAmount = document.querySelectorAll('.section3Project').length
+const projects = document.querySelector('.section3Projects')
+const leftBtn = document.querySelector('.section3BtnLeft')
+const rightBtn = document.querySelector('.section3BtnRight')
+const projectsAmount = document.querySelectorAll('.section3Project').length
 
+const scrollingWorks = () => {
 	let scrollWorksIndex = 0;
 
+	leftBtn.style.opacity = '0.3'
+
 	leftBtn.addEventListener('click', () => {
+		rightBtn.style.opacity = '1'
 		if (scrollWorksIndex > 0) {
-			scrollWorksIndex--
+			scrollWorksIndex -= 1
 			projects.style.transform = `translateX(-${scrollWorksIndex * 300}px)`;
 		}
+		buttonOpacity(scrollWorksIndex)
 	})
 	rightBtn.addEventListener('click', () => {
-		if (scrollWorksIndex < projectsAmount - 1) {
-			scrollWorksIndex++
+		leftBtn.style.opacity = '1'
+		if (scrollWorksIndex < projectsAmount - 2) {
+			scrollWorksIndex += 1
 			projects.style.transform = `translateX(-${scrollWorksIndex * 300}px)`;
 		}
+		buttonOpacity(scrollWorksIndex)
 	})
+}
+
+function buttonOpacity(scrollWorksIndex) {
+	if (scrollWorksIndex === 0) {
+		leftBtn.style.opacity = '0.3'
+	}
+	if (scrollWorksIndex === projectsAmount - 2) {
+		rightBtn.style.opacity = '0.3'
+	}
 }
 
 export default scrollingWorks
