853aa2dbdb69bbc510c7609226489cbc4c6cfd23
Seregey_Maximov <dev9092336062@gmail.com>
2022-01-19 13:56:20 +0300

    added listener for keyboard in scrollingWorks

--- src/js/scrollingWorks.js
+++ src/js/scrollingWorks.js
@@ -2,37 +2,54 @@ const projects = document.querySelector('.section3Projects')
 const leftBtn = document.querySelector('.section3BtnLeft')
 const rightBtn = document.querySelector('.section3BtnRight')
 const projectsAmount = document.querySelectorAll('.section3Project').length
+let scrollWorksIndex = 0;
 
 const scrollingWorks = () => {
-	let scrollWorksIndex = 0;
 
-	buttonOpacity(scrollWorksIndex)
+	worksButtonOpacity(scrollWorksIndex)
 
-	leftBtn.addEventListener('click', () => {
-		rightBtn.style.opacity = '1'
-		if (scrollWorksIndex > 0) {
-			scrollWorksIndex -= 1
-			projects.style.transform = `translateX(-${scrollWorksIndex * 300}px)`;
+	// push Buttons
+
+	leftBtn.addEventListener('click', () => worksGoLeft())
+	rightBtn.addEventListener('click', () => worksGoRight())
+
+	// push Key Arrows
+
+	document.addEventListener('keyup', (event) => {
+		if (event.key === 'ArrowRight') {
+			worksGoRight()
 		}
-		buttonOpacity(scrollWorksIndex)
-	})
-	rightBtn.addEventListener('click', () => {
-		leftBtn.style.opacity = '1'
-		if (scrollWorksIndex < projectsAmount - 2) {
-			scrollWorksIndex += 1
-			projects.style.transform = `translateX(-${scrollWorksIndex * 300}px)`;
+		if (event.key === 'ArrowLeft') {
+			worksGoLeft()
 		}
-		buttonOpacity(scrollWorksIndex)
 	})
 }
 
-function buttonOpacity(scrollWorksIndex) {
+function worksButtonOpacity(scrollWorksIndex) {
 	if (scrollWorksIndex === 0) {
 		leftBtn.style.opacity = '0.3'
 	}
 	if (scrollWorksIndex === projectsAmount - 2) {
 		rightBtn.style.opacity = '0.3'
 	}
+}
+
+function worksGoRight() {
+	leftBtn.style.opacity = '1'
+	if (scrollWorksIndex < projectsAmount - 2) {
+		scrollWorksIndex += 1
+		projects.style.transform = `translateX(-${scrollWorksIndex * 300}px)`;
+	}
+	worksButtonOpacity(scrollWorksIndex)
+}
+
+function worksGoLeft() {
+	rightBtn.style.opacity = '1'
+	if (scrollWorksIndex > 0) {
+		scrollWorksIndex -= 1
+		projects.style.transform = `translateX(-${scrollWorksIndex * 300}px)`;
+	}
+	worksButtonOpacity(scrollWorksIndex)
 }
 
 export default scrollingWorks
